1c4867c48afa830008c41ba6c285b30840bbaa55
Ben <benjamin.w.fyfe@gmail.com>
2025-04-08 19:35:23 +0100

    feat: make spotify widget more readable

--- src/app/_components/music/Song.tsx
+++ src/app/_components/music/Song.tsx
@@ -47,88 +47,96 @@ export default function SpotifyComponent() {
     fetchSpotifyData();
   }, []);
 
-  return (
-    <div>
-      {songData ? (
-        <>
-          <div className="flex items-center">
-            <h3>Music</h3>
-            <div className="relative ml-auto h-3 w-3 bg-green-600/80 rounded-sm group">
-              <span className="absolute bg-text-base text-sm text-center p-1 rounded-sm w-36 right-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
-                Im listening to this right now!
-              </span>
-            </div>
-          </div>
-          <p>Currently listening to:</p>
-          <div className="relative w-full flex overflow-hidden rounded-sm">
-            <div
-              className="absolute inset-0 bg-center bg-cover rounded-sm blur-sm scale-115 opacity-50"
-              style={{
-                backgroundImage: `url('${songData.cover_image[0].url}')`,
-              }}
-            ></div>
-            <div className="z-10 flex">
-              <Image
-                alt="Spotify cover art"
-                src={songData.cover_image[0].url}
-                width={100}
-                height={100}
-                className="m-2 rounded-md"
-              ></Image>
-              <div className="my-2 flex flex-col justify-center">
-                <p className="font-bold stroke-1">{songData.song_name}</p>
-                <p>
-                  {songData.artists.map((artist) => artist.name).join(', ')}
-                </p>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : recentSongs ? (
-        <div>
-          <div className="flex items-center">
-            <h3>Music</h3>
-            <div className="relative ml-auto h-3 w-3 bg-red-400/80 rounded-sm group">
-              <span className="absolute bg-text-base text-sm text-center p-1 rounded-sm w-36 right-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
-                I'm not currently listening to any music.
-              </span>
-            </div>
-          </div>
-          <p className="mb-2">Recently played:</p>
-          <div className="flex justify-evenly gap-4">
-            {recentSongs.map((song, index) => (
-              <>
-                <div className="relative w-fit flex overflow-hidden rounded-sm 2xl:flex-1">
-                  <div
-                    className="absolute inset-0 bg-center bg-cover rounded-sm blur-sm scale-115 opacity-50"
-                    style={{
-                      backgroundImage: `url('${song.cover_image[0].url}')`,
-                    }}
-                  ></div>
-                  <div className="z-10 flex flex-col">
-                    <Image
-                      alt="Spotify cover art"
-                      src={song.cover_image[0].url}
-                      width={100}
-                      height={100}
-                      className="m-2 rounded-md"
-                    ></Image>
-                    <div className="hidden 2xl:block m-2 flex flex-col justify-center">
-                      <p className="font-bold stroke-1">{song.song_name}</p>
-                      <p>
-                        {song.artists.map((artist) => artist.name).join(', ')}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                {/* <div key={song.song_name}>{song.song_name}</div> */}
-              </>
-            ))}
+  function renderNowPlaying(songData: SongData) {
+    return (
+      <>
+        <div className="flex items-center">
+          <h3>Music</h3>
+          <div className="relative ml-auto h-3 w-3 bg-green-600/80 rounded-sm group">
+            <span className="absolute bg-text-base text-sm text-center p-1 rounded-sm w-36 right-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+              Im listening to this right now!
+            </span>
           </div>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+        <p>Currently listening to:</p>
+        <div className="relative w-full flex overflow-hidden rounded-sm">
+          <div
+            className="absolute inset-0 bg-center bg-cover rounded-sm blur-sm scale-115 opacity-50"
+            style={{
+              backgroundImage: `url('${songData.cover_image[0].url}')`,
+            }}
+          ></div>
+          <div className="z-10 flex">
+            <Image
+              alt="Spotify cover art"
+              src={songData.cover_image[0].url}
+              width={100}
+              height={100}
+              className="m-2 rounded-md"
+            ></Image>
+            <div className="my-2 flex flex-col justify-center">
+              <p className="font-bold stroke-1">{songData.song_name}</p>
+              <p>{songData.artists.map((artist) => artist.name).join(', ')}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  function renderRecentSongs(recentSongs: RecentSongs) {
+    return (
+      <div>
+        <div className="flex items-center">
+          <h3>Music</h3>
+          <div className="relative ml-auto h-3 w-3 bg-red-400/80 rounded-sm z-20 group">
+            <span className="absolute bg-text-base text-sm text-center p-1 rounded-sm w-36 right-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+              I'm not currently listening to any music.
+            </span>
+          </div>
+        </div>
+        <p className="mb-2">Recently played:</p>
+        <div className="flex justify-evenly gap-4">
+          {recentSongs.map((song) => (
+            <div
+              key={song.song_name}
+              className="relative w-fit flex overflow-hidden rounded-sm 2xl:flex-1"
+            >
+              <div
+                className="absolute inset-0 bg-center bg-cover rounded-sm blur-sm scale-115 opacity-50"
+                style={{
+                  backgroundImage: `url('${song.cover_image[0].url}')`,
+                }}
+              ></div>
+              <div className="z-10 flex flex-col w-full">
+                <Image
+                  alt="Spotify cover art"
+                  src={song.cover_image[0].url}
+                  width={100}
+                  height={100}
+                  className="m-2 2xl:mx-auto 2xl:mt-2 rounded-md"
+                ></Image>
+                <div className="hidden 2xl:block m-2 flex flex-col justify-center">
+                  <p className="font-bold stroke-1">{song.song_name}</p>
+                  <p>{song.artists.map((artist) => artist.name).join(', ')}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  let spotifyOutput;
+
+  if (songData) {
+    spotifyOutput = renderNowPlaying(songData);
+  } else if (recentSongs) {
+    spotifyOutput = renderRecentSongs(recentSongs);
+  } else {
+    spotifyOutput = <p>Loading...</p>;
+  }
+
+  return <div>{spotifyOutput}</div>;
 }
